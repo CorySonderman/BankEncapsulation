805f6fe00325ee3c594e93d6996495f754834f31
CorySonderman <sonderman2000@yahoo.com>
2023-10-08 11:44:53 -0500

    completed exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace BankEncapsulation
+﻿using System.Runtime.InteropServices;
+
+namespace BankEncapsulation
 {
     public class Program
     {
+        //In the main method of your application, create a new instance of the
+        //BankAccount class. Allow the user of the application to Deposit money and
+        //retrieve their balance through the console.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much is your deposit?");
+            var depositAmount = double.Parse(Console.ReadLine());
+            account.Deposit(depositAmount);
+            Console.WriteLine($"Your new balance is $ {account.GetBalance()}.");
         }
     }
 }
